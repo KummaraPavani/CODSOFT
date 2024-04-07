360111ed0591a955de6836dc6795ba45542ad1a4
Pavani1221 <113250900+Pavani1221@users.noreply.github.com>
2024-04-07 22:53:11 +0530

    Add files via upload

---
+++ randomNumber.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<random>//for generating the random number
+int main()
+{
+    int ip,rn;
+    
+    std::random_device rd;
+    //generating the seed value and passing it to the engine
+    std::mt19937 eng(rd());
+    std::uniform_int_distribution<>dis(55,60);
+    rn=dis(eng);
+    //using do while to take i/p continuously
+
+    do
+    {
+    std::cout<<"enter the number:";
+    std::cin>>ip;
+    if(ip<rn)
+    std::cout<<"too small, enter again\n";
+    else if(ip>rn)
+    std::cout<<"too large enter again\n";
+    }while(ip!=rn);
+
+    std::cout<<"congratulations!!! you guessed the number :)";
+    return 0;
+}
